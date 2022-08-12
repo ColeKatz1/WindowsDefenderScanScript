dc80021cbfc065399803646a6d4ce93d72794a50
ColeKatz1 <84477747+ColeKatz1@users.noreply.github.com>
2022-08-12 12:38:54 -0400

    Update QuickScan_Script.py

--- QuickScan_Script.py
+++ QuickScan_Script.py
@@ -1,3 +1,7 @@
 import os
 
-os.system('cmd /c ""%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType 1"')
+def runWDScan(scanType):
+  os.system('cmd /c ""%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType "' + scanType)
+
+runWDScan("1") #this runs a quick scan
+runWDScan("2") #this runs a full scan
